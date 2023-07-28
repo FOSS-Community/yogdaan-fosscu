da4c092de0466b5f6f728b4310281c6ac324a8b9
Kanishk Pachauri <itskanishkp.py@gmail.com>
2023-07-29 03:24:52 +0530

    Fixed the Database migration problem

--- app/alembic/env.py
+++ app/alembic/env.py
@@ -61,11 +61,13 @@ def run_migrations_online() -> None:
     and associate a connection with the context.
 
     """
-    connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
-    )
+    from app.db.database import engine
+    # connectable = engine_from_config(
+    #     config.get_section(config.config_ini_section, {}),
+    #     prefix="sqlalchemy.",
+    #     poolclass=pool.NullPool,
+    # )
+    connectable = engine
 
     with connectable.connect() as connection:
         context.configure(
